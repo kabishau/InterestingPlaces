d91b118102056f4404bcc49af203a6eb66d7bade
AlekseyK <aleksey.kabishau@gmail.com>
2019-05-26 13:13:27 -0400

    changing indentation

--- InterestingPlaces/AppDelegate.swift
+++ InterestingPlaces/AppDelegate.swift
@@ -2,13 +2,13 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-  var window: UIWindow?
-
-
-  private func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
     
-    return true
-  }
+    var window: UIWindow?
+    
+    
+    private func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        
+        return true
+    }
 }
 
--- InterestingPlaces/ViewController.swift
+++ InterestingPlaces/ViewController.swift
@@ -2,101 +2,102 @@ import UIKit
 import CoreLocation
 
 class ViewController: UIViewController {
-
-  @IBOutlet weak var placeName: UILabel!
-  @IBOutlet weak var locationDistance: UILabel!
-  @IBOutlet weak var placeImage: UIImageView!
-  var placesViewController: PlaceScrollViewController?
-  var locationManager: CLLocationManager?
-  var previousLocation: CLLocation?
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    if let childViewController = children.first as? PlaceScrollViewController {
-      placesViewController = childViewController
+    
+    @IBOutlet weak var placeName: UILabel!
+    @IBOutlet weak var locationDistance: UILabel!
+    @IBOutlet weak var placeImage: UIImageView!
+    var placesViewController: PlaceScrollViewController?
+    
+    var locationManager: CLLocationManager?
+    var previousLocation: CLLocation?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if let childViewController = children.first as? PlaceScrollViewController {
+            placesViewController = childViewController
+        }
+        loadPlaces()
+        
+        locationManager = CLLocationManager()
+        locationManager?.delegate = self
+        locationManager?.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        
+        
     }
-    loadPlaces()
     
-    locationManager = CLLocationManager()
-    locationManager?.delegate = self
-    locationManager?.desiredAccuracy = kCLLocationAccuracyHundredMeters
+    func selectPlace() {
+        print("place selected")
+    }
     
-    
-  }
-
-  func selectPlace() {
-    print("place selected")
-  }
-  
     // check the status of
-  @IBAction func startLocationService(_ sender: UIButton) {
-    if CLLocationManager.authorizationStatus() == .authorizedAlways || CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
-      
-      activateLocationServices()
-      
-    } else {
-      locationManager?.requestWhenInUseAuthorization()
+    @IBAction func startLocationService(_ sender: UIButton) {
+        if CLLocationManager.authorizationStatus() == .authorizedAlways || CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
+            
+            activateLocationServices()
+            
+        } else {
+            locationManager?.requestWhenInUseAuthorization()
+        }
     }
-  }
-  
-  private func activateLocationServices() {
-    locationManager?.startUpdatingLocation()
-  }
-  
-  func loadPlaces() {
     
-    guard let entries = loadPlist() else { fatalError("Unable to load data") }
-    
-    for property in entries {
-      guard let name = property["Name"] as? String,
-            let latitude = property["Latitude"] as? NSNumber,
-            let longitude = property["Longitude"] as? NSNumber,
-            let image = property["Image"] as? String else { fatalError("Error reading data") }
-      print("name: \(name)")
-      print("latitude: \(latitude)")
-      print("longitude: \(longitude)")
-      print("image: \(image)")
+    private func activateLocationServices() {
+        locationManager?.startUpdatingLocation()
     }
-  }
-  
-  private func loadPlist() -> [[String: Any]]? {
-    guard let plistUrl = Bundle.main.url(forResource: "Places", withExtension: "plist"),
-      let plistData = try? Data(contentsOf: plistUrl) else { return nil }
-    var placedEntries: [[String: Any]]? = nil
     
-    do {
-      placedEntries = try PropertyListSerialization.propertyList(from: plistData, options: [], format: nil) as? [[String: Any]]
-    } catch {
-      print("error reading plist")
+    func loadPlaces() {
+        
+        guard let entries = loadPlist() else { fatalError("Unable to load data") }
+        
+        for property in entries {
+            guard let name = property["Name"] as? String,
+                let latitude = property["Latitude"] as? NSNumber,
+                let longitude = property["Longitude"] as? NSNumber,
+                let image = property["Image"] as? String else { fatalError("Error reading data") }
+            print("name: \(name)")
+            print("latitude: \(latitude)")
+            print("longitude: \(longitude)")
+            print("image: \(image)")
+        }
     }
-    return placedEntries
-  }
+    
+    private func loadPlist() -> [[String: Any]]? {
+        guard let plistUrl = Bundle.main.url(forResource: "Places", withExtension: "plist"),
+            let plistData = try? Data(contentsOf: plistUrl) else { return nil }
+        var placedEntries: [[String: Any]]? = nil
+        
+        do {
+            placedEntries = try PropertyListSerialization.propertyList(from: plistData, options: [], format: nil) as? [[String: Any]]
+        } catch {
+            print("error reading plist")
+        }
+        return placedEntries
+    }
 }
 
 extension ViewController: CLLocationManagerDelegate {
-  
-  func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
     
-    if status == .authorizedWhenInUse || status == .authorizedAlways {
-      activateLocationServices()
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        
+        if status == .authorizedWhenInUse || status == .authorizedAlways {
+            activateLocationServices()
+        }
     }
-  }
-  
+    
     // after activation app starts receiving locations
-  func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-    
-    if previousLocation == nil {
-      previousLocation = locations.first
-    } else {
-      guard let latest = locations.first else { return }
-      let distanceInMeters = previousLocation?.distance(from: latest) ?? 0
-      print("distance in meters: \(distanceInMeters)")
-      previousLocation = latest
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        if previousLocation == nil {
+            previousLocation = locations.first
+        } else {
+            guard let latest = locations.first else { return }
+            let distanceInMeters = previousLocation?.distance(from: latest) ?? 0
+            print("distance in meters: \(distanceInMeters)")
+            previousLocation = latest
+        }
+        
+        
     }
     
     
-  }
-  
-  
 }
 
